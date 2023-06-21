37f9de81e62a335f1d547c4cd4982316992d693e
regier21 <ryanr2302+github@gmail.com>
2023-06-20 18:42:41 -0700

    Let user configure figure prefix and added semicolons

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict'
 
-module.exports = function cap_plugin(md) {
+const defaults = {
+  prefix: "◎"
+};
+
+module.exports = function cap_plugin(md, opts={}) {
   // console.log(md.renderer.rules);
-  var old = md.renderer.rules.image
+  const settings = Object.assign({}, defaults, opts);
+  var old = md.renderer.rules.image;
 
   md.renderer.rules.image = function (tokens, idx, options, env, self) {
     if (tokens[idx].attrs[2]) {
-      let attrs = tokens[idx].attrs
-      let title = attrs[2][1]
-      let src = attrs[0][1]
-      let alt = tokens[idx].content
-      return `<figure><img src="${src}" alt="${alt}" title="${title}" /><figcaption align="center"><small>◎ ${title}</small></figcaption></figure>`
+      let attrs = tokens[idx].attrs;
+      let title = attrs[2][1];
+      let src = attrs[0][1];
+      let alt = tokens[idx].content;
+      return `<figure><img src="${src}" alt="${alt}" title="${title}" /><figcaption align="center"><small>${settings.prefix} ${title}</small></figcaption></figure>`;
     }
-    return old(tokens, idx, options, env, self)
-  }
-}
+    return old(tokens, idx, options, env, self);
+  };
+};
